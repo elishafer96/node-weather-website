872ba7d1d6217fda46b14457f33e79e3416a25e0
Eli Shafer <eli.shafer96@gmail.com>
2020-11-02 10:42:08 -0800

    Init commit.

---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=95fc432542103c5311d774faaf19d1ca&query=${encodeURIComponent(
+    latitude
+  )},${encodeURIComponent(longitude)}&units=f`;
+
+  request({ url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback("Unable to connect to weather service!");
+    } else if (body.error) {
+      callback("Unable to find location.");
+    } else {
+      const { current } = body;
+      callback(
+        undefined,
+        "It is currently " +
+          current.weather_descriptions[0].toLowerCase() +
+          " and " +
+          current.temperature +
+          " degrees out.\nIt feels like " +
+          current.feelslike +
+          " degrees."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
